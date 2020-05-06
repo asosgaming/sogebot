610c56e3d27dc07087fdb52910b85fa96ad00e07
Reesey275 <reesey275@gmail.com>
2020-05-06 08:18:13 -0400

    updated paths for compress file.

--- tools/compress-sogebot-test.sh
+++ tools/compress-sogebot-test.sh
@@ -5,13 +5,13 @@ echo Please Enter sogeBot build verion:
 
 read version
 
-rm -R /mnt/d/TheAngryGamerDrive/Development/sogeBot-release/*.sogeBot.tar.gz
+rm -R *.sogeBot.tar.gz
 
-tar -czf /mnt/d/TheAngryGamerDrive/Development/sogeBot-release/$version.sogeBot.tar.gz -C /mnt/d/TheAngryGamerDrive/Development sogeBot
+tar -czf $version.sogeBot.tar.gz -C /Development sogeBot
 
-ssh chad@reesey275 "rm -f /home/chad/current-build/*.tar.gz"
+ssh chad@reesey275 "rm -f ~/current-build/*.tar.gz"
 
-scp /mnt/d/TheAngryGamerDrive/Development/sogeBot-release/$version.sogeBot.tar.gz chad@reesey275.asosgaming.net:/home/chad/current-build/
+scp /Development/sogeBot-release/$version.sogeBot.tar.gz chad@reesey275.asosgaming.net:~/current-build/
 
 
-ssh chad@reesey275 "mv /home/chad/sogeBot /home/chad/$(date +"%Y_%m_%d_%I_%M_%p")_sogeBot && tar -xvf /home/chad/current-build/* && cp ~/.env ~/sogeBot/ && bash ./start.sh" 
+ssh chad@reesey275 "mv ~/sogeBot ~/$(date +"%Y_%m_%d_%I_%M_%p")_sogeBot && tar -xvf ~/current-build/* && cp ~/.env ~/sogeBot/ && bash ./start.sh" 
